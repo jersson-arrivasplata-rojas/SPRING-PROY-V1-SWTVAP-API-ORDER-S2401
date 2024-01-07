d9e36e5ebef6afdd79ecd8456356c9a505b66060
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-01-07 00:50:59 -0500

    chore: Proyect init

---
+++ src/test/java/com/jersson/arrivasplata/swtvap/api/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jersson.arrivasplata.swtvap.api.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
