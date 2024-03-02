6cb8237d1b6f150c558092311c60c751d7de3615
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-03-02 08:07:41 -0500

    Add files

--- src/main/java/com/jersson/arrivasplata/swtvap/api/order/enums/OrderStatus.java
+++ src/main/java/com/jersson/arrivasplata/swtvap/api/order/enums/OrderStatus.java
@@ -1,27 +1,27 @@
 package com.jersson.arrivasplata.swtvap.api.order.enums;
 
 public enum OrderStatus {
-    PENDING_PAYMENT("The order has been placed by the customer, but payment has not been completed."),
-    PAYMENT_RECEIVED("Payment for the order has been received and confirmed."),
-    PROCESSING("The products in the order are being gathered and prepared for shipment."),
-    IN_TRANSIT("The order has been shipped and is on its way to the specified shipping address."),
-    DELIVERED("The order has been successfully delivered to the customer."),
-    CANCELED_BY_CUSTOMER("The customer has requested to cancel the order before shipment."),
-    CANCELED_BY_COMPANY("The company has canceled the order for internal reasons such as lack of stock, payment issues, etc."),
-    WAITING_FOR_PAYMENT_CONFIRMATION("The order has been placed, but the company is waiting for payment confirmation before proceeding."),
-    REFUNDED("The order has been refunded to the customer, either partially or in full."),
-    CUSTOMS_PROCESSING("The order is undergoing customs clearance before international shipment."),
-    WAITING_FOR_DOCUMENTATION("The order is pending necessary documentation for export."),
-    INTERNATIONAL_TRANSIT("The order has left the country and is en route to its international destination."),
-    HELD_IN_CUSTOMS("The order has arrived at the destination country but is being held in customs for inspection or other procedures.");
+    PENDING_PAYMENT("La orden ha sido realizada por el cliente, pero el pago aún no se ha completado."),
+    PAYMENT_RECEIVED("El pago de la orden ha sido recibido y confirmado."),
+    PROCESSING("Los productos de la orden están siendo reunidos y preparados para su envío."),
+    IN_TRANSIT("La orden ha sido enviada y está en camino hacia la dirección de envío especificada."),
+    DELIVERED("La orden ha sido entregada con éxito al cliente."),
+    CANCELED_BY_CUSTOMER("El cliente ha solicitado cancelar la orden antes de su envío."),
+    CANCELED_BY_COMPANY("La empresa ha cancelado la orden por razones internas, como falta de stock, problemas de pago, etc."),
+    WAITING_FOR_PAYMENT_CONFIRMATION("La orden se ha realizado, pero la empresa está esperando la confirmación del pago antes de proceder."),
+    REFUNDED("La orden ha sido reembolsada al cliente, ya sea parcial o totalmente."),
+    CUSTOMS_PROCESSING("La orden se encuentra en proceso de despacho aduanero antes de su envío internacional."),
+    WAITING_FOR_DOCUMENTATION("La orden está pendiente de la documentación necesaria para la exportación."),
+    INTERNATIONAL_TRANSIT("La orden ha salido del país y se encuentra en camino hacia su destino internacional."),
+    HELD_IN_CUSTOMS("La orden ha llegado al país de destino pero está retenida en la aduana para inspección u otros trámites.");
 
-    private final String description;
+    private final String descripcion;
 
-    OrderStatus(String description) {
-        this.description = description;
+    OrderStatus(String descripcion) {
+        this.descripcion = descripcion;
     }
 
-    public String getDescription() {
-        return description;
+    public String getDescripcion() {
+        return descripcion;
     }
 }
